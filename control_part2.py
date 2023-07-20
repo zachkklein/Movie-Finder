2781ed56906f6a51172e2afd9ff0bfd715f17f78
zachkklein <zachkklein@gmail.com>
2023-07-20 09:55:35 -0400

    WORKING, some css fixes needed

--- control_part2.py
+++ control_part2.py
@@ -3,19 +3,21 @@ import pandas as pd
 user_1=moviedata()
 def mainpart(user_ing, user_inc, user_date):
     
-
-    genresls=user_ing.lstrip().rstrip().split(",")
-    castls=user_inc.lstrip().rstrip().split(",")
-    datels=int(user_date.lstrip().rstrip()[0:4])
-    wanted_df=moviedata.search(user_1.movies_df,Only=False,genres=genresls,cast=castls)
-    secondpart=moviedata.search(user_1.movies_df,Only=False,genres=genresls)
-    final_one=pd.concat([wanted_df,secondpart],axis=0)
-    final_one.drop_duplicates(subset=['id'],keep='first',inplace=True)
-    moviedata.forsort(wanted_df,datels)
-    if wanted_df.shape[0]>100:
-        return wanted_df.head(100)
-    else:
-        return wanted_df
+    try:
+        genresls=user_ing.lstrip().rstrip().split(",")
+        castls=user_inc.lstrip().rstrip().split(",")
+        datels=int(user_date.lstrip().rstrip()[0:4])
+        wanted_df=moviedata.search(user_1.movies_df,Only=False,genres=genresls,cast=castls)
+        secondpart=moviedata.search(user_1.movies_df,Only=False,genres=genresls)
+        final_one=pd.concat([wanted_df,secondpart],axis=0)
+        final_one.drop_duplicates(subset=['id'],keep='first',inplace=True)
+        moviedata.forsort(wanted_df,datels)
+        if wanted_df.shape[0]>100:
+            return wanted_df.head(100)
+        else:
+            return wanted_df
+    except:
+        return 'Error'
 
 
 def df_tolist(df:pd.DataFrame):
